6e158dd52a83263161a407c9b47ec3220fadf354
Kevin Jung <kev.jung.ii@gmail.com>
2021-01-14 02:45:14 -0500

    commit tree 5f310e07e2df3392d403ce3ec3a7cbba6402d7f9

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.empty?
+    nil
+  else
+    lowest_value = 100
+    name_hash.each do |key, value|
+      if value < lowest_value
+        lowest_value = value
+      end
+    end
+    lowest_key = name_hash.collect do |key, value|
+      if value == lowest_value
+        return key
+      end
+    end
+  end
+  lowest_key
 end
